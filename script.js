5e87a760727794c77c9c0d521dd86e69bf7863e5
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-09-19 12:44:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(n) {
+	if((n % 100 == 0) && (n % 400 != 0)){
+		return 365;
+	} else if (n % 4 == 0) {
+		return 366;
+	} else {
+		return 365;
+	}
+}
